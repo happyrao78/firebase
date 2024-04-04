3ce830d3415815f21198bd3b54492bd5ec366b8f
HAPPY YADAV <happy.hy0017@gmail.com>
2024-04-04 22:52:42 +0530

    Included tailwind.

---
+++ main/index.js
@@ -0,0 +1,9 @@
+const input = document.getElementById("input-field");
+const button = document.getElementById("add-button");
+
+button.addEventListener("click", function (){
+    const value = input.value;
+        console.log(value);
+}
+
+)
